1588cf82f34749211bad18c4b03a8abb45d47d04
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-07 02:02:19 +0530

    added module to main js file

---
+++ main.js
@@ -0,0 +1,12 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const divide = require('./divide');
+const multiply = require('./multiply');
+
+
+let a = 10;
+let b = 5;
+add(a , b);
+subtract(a , b);
+divide(a , b);
+multiply(a , b);
